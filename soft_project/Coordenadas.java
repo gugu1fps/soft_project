a70bbbb2ff052978cc5a9fade5b99de70642741e
gugu1fps <140581649+gugu1fps@users.noreply.github.com>
2025-07-07 14:38:55 -0300

    Delete soft_project directory

--- soft_project/Coordenadas.java
+++
@@ -1,9 +0,0 @@
-public class Coordenadas {
-    private float latitude;
-    private float longitude;
-
-    public Coordenadas(float latitude, float longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-}
